28dc24d12d76c7fbf66e970a426f145ffafcf261
MarioNadalCasa <220056@iessanalberto.com>
2023-11-29 18:26:36 +0100

    Finalizado con comentarios.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -48,6 +48,7 @@ public class Main {
     public static void listarClientesNumArticulos(){
         List<ClientesEntity> listaClientes = em.createQuery("from ClientesEntity ", ClientesEntity.class).getResultList();
         int i = 0;
+        //Listo todos los clientes que exitsten con el numero de Articulos del cliente
         System.out.println("Lista clientes: ");
         for(ClientesEntity clientes: listaClientes){
             int numArticulosClienteTotal = 0;
@@ -55,6 +56,7 @@ public class Main {
             i++;
             System.out.print(i+". " + clientes.getNombre());
             List<VentaprodEntity> numArticulosClinete = em.createQuery("from VentaprodEntity where idCliente = ?1 ", VentaprodEntity.class).setParameter(1,clientes.getId()).getResultList();
+            //Sumo todas las unidades de todas las ventas del cliente para saber cuantos articulos ha comprado en total
             for(VentaprodEntity numArticuloCliente: numArticulosClinete){
                 numArticulosClienteTotal += numArticuloCliente.getUnidades();
             }
@@ -65,6 +67,7 @@ public class Main {
     public static void listarProductosNumUnidades(){
         List<ProductosEntity> listaProductos = em.createQuery("from ProductosEntity ", ProductosEntity.class).getResultList();
         int i = 0;
+        //Liston todos los producto
         System.out.println("Lista Productos: ");
         for(ProductosEntity productos: listaProductos){
             int numProductosVendidosTotal = 0;
@@ -72,6 +75,7 @@ public class Main {
             i++;
             System.out.print(i+". "+ productos.getDescripcion());
             List<VentaprodEntity> numProductosVendidos = em.createQuery("from VentaprodEntity where idProducto = ?1 ", VentaprodEntity.class).setParameter(1,productos.getId()).getResultList();
+            //Sumo todos los productos que ha vendido teniendo en cuenta las unidades
             for(VentaprodEntity numProductoVendido: numProductosVendidos){
                 numProductosVendidosTotal += numProductoVendido.getUnidades();
             }
@@ -83,17 +87,20 @@ public class Main {
         int clienteIntroducido = 0, productoIntroducido = 0, unidadesIntroducidas;
         Date fechaIntroducida;
         boolean validarCliente=false, validarProducto=false;
-        //Listo los clientes para saber cúal ha hecho la compra
+        //Hasta que no se  introduzca un cliente válido se seguirá pidiendo el cliente
         while(!validarCliente) {
             List<ClientesEntity> listaClientes = em.createQuery("from ClientesEntity ", ClientesEntity.class).getResultList();
             int i = 0;
+            //Listo todos los clientes que hay
             System.out.println("Lista clientes: ");
             for (ClientesEntity clientes : listaClientes) {
                 //Número de cliente en modo de contador
                 i++;
                 System.out.println(i + ". " + clientes.getNombre() + " ID: " + clientes.getId());
             }
+            //Hago que introduzca el id del cliente que le aparece al lado del nombre del cliente en la lista anterior
             clienteIntroducido = Leer.introduceEntero("Introduce el id del cliente que ha hecho la compra");
+            //Esta es la función que valida que el id sea de un cliente real
             for(ClientesEntity clientes : listaClientes){
                 if(clientes.getId() == (short) clienteIntroducido){
                     validarCliente=true;
@@ -103,6 +110,7 @@ public class Main {
                 System.out.println("Introduce un id válido de la lista anterior");
             }
         }
+        //hago lo mismo pero con el producto a introducir
         while(!validarProducto){
             //Listo los productos para saber cúal ha comprado el cliente
             List<ProductosEntity> listaProductos = em.createQuery("from ProductosEntity ", ProductosEntity.class).getResultList();
@@ -123,16 +131,24 @@ public class Main {
                 System.out.println("Introduce un ID válido de la lista anterior");
             }
         }
+        //introduce las unidades que ha vendido
         unidadesIntroducidas = Leer.introduceEntero("Inroduce el número de unidades que se han comprado");
+        //introduce la fecha ya que puede haber sido en el fin de semana o anterior no hace falta que haya sido hoy si o si
         fechaIntroducida = Leer.introduceDateSQL("Introduce la fecha de la venta (yyyy-mm-dd)");
+        //Empiezo a coger los datos que se tendrán que actualizar en la base de datos
         transaction.begin();
+        //Creo una Venta nuevo donde voy a añadir todos los datos que ha introducido el usuario
         VentaprodEntity insertVenta = new VentaprodEntity();
+        //Introduzco todos los datos
         insertVenta.setIdCliente((short) clienteIntroducido);
         insertVenta.setIdProducto((short) productoIntroducido);
         insertVenta.setUnidades((short) unidadesIntroducidas);
         insertVenta.setFecha(fechaIntroducida);
+        //Una vez metidos todos los datos inserto la venta en la base de datos
         em.persist(insertVenta);
+        //hago que se actualize desde el commit begin en la base de datos
         transaction.commit();
+        //Conmfirmo al usuario que se ha podido introducir la venta ya que nada se ve en consola
         System.out.println("-------------------------------------------------------------------------------");
         System.out.println("Venta introducida correctamente");
         System.out.println("-------------------------------------------------------------------------------");
@@ -144,6 +160,7 @@ public class Main {
         Double importeTotal = 0.0;
         String cliente, nombreClienteSeleccionado = "";
         boolean validacionCliente = false;
+        //valido el cliente del que quiere ver las ventas
         while(!validacionCliente) {
             i=0;
             System.out.println("Lista clientes: ");
@@ -161,20 +178,25 @@ public class Main {
                 }
             }
         }
+        //Hago el ticket bonito como se pide en la imagen
         List<VentaprodEntity> listaVentasCliente = em.createQuery("from VentaprodEntity where idCliente = ?1", VentaprodEntity.class).setParameter(1,idClienteSeleccionado).getResultList();
         System.out.println("---------------------------------------");
         System.out.println("Ventas del cliente: " + nombreClienteSeleccionado);
         System.out.println("---------------------------------------");
+        //Primero recorro todas las ventas para poder hacer cada venta con sus datos
         for(VentaprodEntity ventaCliente: listaVentasCliente){
             System.out.println("Venta: " + ventaCliente.getId()+", Fecha venta: " + ventaCliente.getFecha());
+            //Dentro de cada venta tengo que coger el producto que se ha vendido en esa venta exacta
             List<ProductosEntity> listaProductos = em.createQuery("from ProductosEntity where id = ?1 ", ProductosEntity.class).setParameter(1,ventaCliente.getIdProducto()).getResultList();
             for(ProductosEntity producto: listaProductos) {
                 System.out.println("    Producto: " + producto.getDescripcion());
                 System.out.println("    Cantidad: " + ventaCliente.getUnidades() + " PVP: " + producto.getPrecio());
                 System.out.println("    Importe: " + ventaCliente.getUnidades()*producto.getPrecio());
+                //sumo el importe de cada venta para tener el total para el final
                 importeTotal = importeTotal + ventaCliente.getUnidades()*producto.getPrecio();
             }
         }
+        //Acabo el ticket mostrando datos totales al usuario por consola
         System.out.println("---------------------------------------");
         System.out.println("Número total de ventas: " + listaVentasCliente.size());
         System.out.println("Importe Total: " + importeTotal);
